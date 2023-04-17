f302e7be3fa30a0575d18506bcb530855c2ec109
DanNObi <126242947+DanNObi@users.noreply.github.com>
2023-04-17 18:44:42 +0200

    Update ChampionshipClasses.js

--- Scripts/Classes/ChampionshipClasses.js
+++ Scripts/Classes/ChampionshipClasses.js
@@ -21,6 +21,42 @@ class Championship {
     }
 }
 
+class Season {
+    constructor(teams, groups, edition) {
+        this.#teams = teams;
+        this.#edition = edition;
+        this.#groups = groups;
+    }
+
+    get teams() {
+        return this.#teams;
+    }
+    set teams(teams) {
+        this.#teams = teams;
+    }
+
+    get edition() {
+        return this.#edition;
+    }
+    set edition(edition) {
+        this.#edition = edition;
+    }
+
+    get groups() {
+        return this.#groups;
+    }
+    set groups(groups) {
+        this.#groups = groups;
+    }
+
+    getMatchDayRanking(day) {
+        
+    }
+    getRanking() {
+        
+    }
+}
+
 class Group {
     constructor(days) {
         this.#days = days;
