3b5e8b72a0908203390e9711bc5a68b3668856fd
DanNObi <126242947+DanNObi@users.noreply.github.com>
2023-04-19 13:51:15 +0200

    Update TeamClasses.js

--- Scripts/Classes/TeamClasses.js
+++ Scripts/Classes/TeamClasses.js
@@ -206,3 +206,28 @@ class Address{
         this.#number = number;
     }
 }
+
+class Address{
+    #street;
+    #number;
+    constructor(street,number){
+        this.#street = street;
+        this.#number = number;
+    }
+
+    get street(){
+        return this.#street;
+    }
+
+    set street(street){
+        this.#street = street;
+    }
+
+    get number(){
+        return this.#number;
+    }
+
+    set number(number){
+        this.#number = number;
+    }
+}
