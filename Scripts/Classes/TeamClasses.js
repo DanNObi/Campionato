0faea347b5fd9376615e437acb10defa2748e5e0
DanNObi <126242947+DanNObi@users.noreply.github.com>
2023-04-19 14:56:59 +0200

    Ho trasportato la classe Trophy sul modulo

--- Scripts/Classes/TeamClasses.js
+++ Scripts/Classes/TeamClasses.js
@@ -231,3 +231,34 @@ export class Address{
         this.#number = number;
     }
 }
+
+export class Trophy {
+    #name; #edition;
+    
+    /**
+     * @param {String} name
+     * @param {int} edition
+     */
+
+    constructor(name, edition){
+        this.#name = name;
+        this.#edition = edition;
+
+    }
+
+    get name(){
+        return this.#name;
+    }
+
+    set name(name){
+        this.#name = name;
+    }
+    
+    get edition(){
+        return this.#name;
+    }
+
+    set edition(edition){
+        this.#edition = edition;
+    }
+}
