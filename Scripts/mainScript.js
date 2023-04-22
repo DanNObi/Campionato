fc3d200e08b6187d4893f23e2bbf44a0a447c494
DanNObi <126242947+DanNObi@users.noreply.github.com>
2023-04-22 20:56:31 +0200

    Impostato le istruzioni di import

--- Scripts/mainScript.js
+++ Scripts/mainScript.js
@@ -1,1 +1,6 @@
-
+import * as ChampionshipMod from './Classes/ChampionshipClasses.js'
+import * as MatchMod from './Classes/MatchClasses.js'
+import * as TeamMod from './Classes/TeamClasses.js'
+import * as PersonMod from './Classes/PersonClasses.js'
+// Per usare una classe usare [Match/Team/ecc..]Mod.[NomeCosa]
+// Write something, magic man!
