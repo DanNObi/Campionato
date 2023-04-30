918e23ea63e0a144202f052b6edb6c13ffcce124
DanNObi <126242947+DanNObi@users.noreply.github.com>
2023-04-30 17:41:21 +0200

    Ho aggiunto il file con gli account

---
+++ Scripts/accounts.js
@@ -0,0 +1,30 @@
+const accounts = [{
+  "username": "Floriano Noto",
+  "password": "cccc",
+  "type": "Presidente",
+  "team": "U.S. Catanzaro"
+},
+{
+  "username": "Antonio Calabro",
+  "password": "cccc",
+  "type": "Allenatore",
+  "team": "U.S. Catanzaro"
+},
+{
+  "username": "Gianni Vrenna",
+  "password": "crot",
+  "type": "Presidente",
+  "team": "F.C. Crotone"
+},
+{
+  "username": "Aimo Dana",
+  "password": "eeee",
+  "type": "Allenatore",
+  "team": "Cesena F.C."
+},
+{
+  "username": "Maurizio Ciampi",
+  "password": "super",
+  "type": "Arbitro",
+  "team": "FIGC"
+}];
